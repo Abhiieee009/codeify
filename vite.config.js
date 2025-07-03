a96de4b68668788a8474d97813fde12670e18ee1
ABHIJEET DANDE <abhijeetdande@ABHIEES-MacBook-Air.local>
2025-07-03 19:02:54 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:"/codeify"
+  base:'/codeify/',
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase from 500KB to 1000KB
+  }
+  
 })
